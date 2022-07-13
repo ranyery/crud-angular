a3cfe997dc4745c1c6737a44b927ac2d61b8b422
ranyery <ranyerysc@gmail.com>
2022-07-12 21:58:35 -0300

    feat: adiciona componente de navegacao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,25 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/template/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component';
+
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
