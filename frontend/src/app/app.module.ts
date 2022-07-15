b27788f0ee4e20e6ca14da682781520bcdc0aa47
ranyery <ranyerysc@gmail.com>
2022-07-15 13:55:24 -0300

    feat: adiciona funcionalidade para editar produtos atraves do id

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MatSortModule } from "@angular/material/sort";
 
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -50,6 +51,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
