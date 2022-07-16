fac0d9df9ccb5770df873466df31eccb8a9eb3ff
ranyery <ranyerysc@gmail.com>
2022-07-15 21:41:35 -0300

    feat: adiciona service para realizar alteracao de informacoes do cabecalho

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HeaderService } from "./header.service";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  constructor(private headerService: HeaderService) {}
+
+  ngOnInit(): void {}
+
+  public get title(): string {
+    return this.headerService.headerData.title;
+  }
+
+  public get icon(): string {
+    return this.headerService.headerData.icon;
+  }
+
+  public get routeUrl(): string {
+    return this.headerService.headerData.routeUrl;
+  }
+}
